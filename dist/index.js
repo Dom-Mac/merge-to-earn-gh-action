7f53401217cc077adf36b8c6c1378ddca0776346
Dom-Mac <domenicochad@gmail.com>
2022-10-11 19:29:32 +0200

    v$npm_config_vtp

--- dist/index.js
+++ dist/index.js
@@ -9692,18 +9692,13 @@ try {
   const slicer = core.getInput("slicer")
   console.log(slicer, "slicer")
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payloadJSON = JSON.stringify(github.context.payload)
   const payload = github.context.payload
-  console.log(`The event payload: ${payloadJSON.inputs}`)
-  console.log(`The event payload: ${payloadJSON["inputs"]}`)
-  console.log(`payload jason: ${payload.inputs}`)
-  console.log(`payload jason: ${payload["inputs"]}`)
 
-  // const address = payload.inputs.address
-  // const slices = payload.inputs.slices
-  // console.log(`Address ${address}!`)
-  // console.log(`Slicer ${slicer}!`)
-  // console.log(`Mint ${slices} slices!`)
+  const address = payload.inputs.address
+  const slices = payload.inputs.slices
+  console.log(`Address ${address}!`)
+  console.log(`Slicer ${slicer}!`)
+  console.log(`Mint ${slices} slices!`)
 } catch (error) {
   core.setFailed(error.message)
 }
