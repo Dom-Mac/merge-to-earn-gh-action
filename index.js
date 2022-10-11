6f762b37cf30362e0e2320ea467f3dd2f61d8921
Dom-Mac <domenicochad@gmail.com>
2022-10-11 23:41:16 +0200

    v$npm_config_vtp

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 const core = require("@actions/core")
 const github = require("@actions/github")
+const { ethers } = require("ethers")
 
 try {
-  // `address` input defined in action metadata file
+  // get inputs from the workflow
   const slicer = core.getInput("slicer")
-  console.log(slicer, "slicer")
-  // Get the JSON webhook payload for the event that triggered the workflow
   const payload = github.context.payload
-
   const address = payload.inputs.address
   const slices = payload.inputs.slices
+
+  // Set provider and signer
+  const provider = new ethers.providers.AlchemyProvider("homestead")
+
+  const testBalance = provider
+    .getBalance("jacopo.eth")
+    .then(() => console.log(testBalance, "testBalance"))
+
+  console.log(testBalance, "testBalance")
+  console.log(slicer, "slicer")
   console.log(`Address ${address}!`)
   console.log(`Slicer ${slicer}!`)
   console.log(`Mint ${slices} slices!`)
+  console.log("payload", payload)
 } catch (error) {
   core.setFailed(error.message)
 }
