29ae0a52cf45180e3ee0ffbf44fb4d34d0898fcd
Dom-Mac <domenicochad@gmail.com>
2022-10-11 17:52:31 +0200

    inputs

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const core = require("@actions/core")
 const github = require("@actions/github")
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput("who-to-greet")
-  console.log(`Hello ${nameToGreet}!`)
-  const time = new Date().toTimeString()
-  core.setOutput("time", time)
+  // `address` input defined in action metadata file
+  const address = core.getInput("address")
+  const slicer = core.getInput("slicer")
+  const slices = core.getInput("slices")
+  console.log(`Address ${address}!`)
+  console.log(`Slicer ${slicer}!`)
+  console.log(`Mint ${slices} slices!`)
+
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`)
