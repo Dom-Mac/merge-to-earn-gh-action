e5f09a7b7dd9dd3f33d179bdf60b81b62a1165ac
Dom-Mac <domenicochad@gmail.com>
2022-10-11 18:53:02 +0200

    take inputs from payload

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ const github = require("@actions/github")
 
 try {
   // `address` input defined in action metadata file
-  const address = core.getInput("address")
   const slicer = core.getInput("slicer")
-  const slices = core.getInput("slices")
-  console.log(`Address ${address}!`)
-  console.log(`Slicer ${slicer}!`)
-  console.log(`Mint ${slices} slices!`)
-
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`)
+
+  const address = payload.inputs.address
+  const slices = payload.inputs.slices
+  console.log(`Address ${address}!`)
+  console.log(`Slicer ${slicer}!`)
+  console.log(`Mint ${slices} slices!`)
 } catch (error) {
   core.setFailed(error.message)
 }
