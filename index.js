d1c9fdb7cb1b54e2ae2d95e76c2113dbbe019be8
Dom-Mac <domenicochad@gmail.com>
2022-10-12 14:36:44 +0200

    v$npm_config_vtp

--- index.js
+++ index.js
@@ -8,28 +8,28 @@ try {
   const slicerAddress = core.getInput("slicer")
   const alchemyApiKey = core.getInput("alchemy_api_key")
   const payload = github.context.payload
-  const address = payload.inputs.address
-  const slices = payload.inputs.slices
+  // const address = payload.inputs.address
+  // const slices = payload.inputs.slices
 
   // Set provider and signer
-  const provider = new ethers.providers.AlchemyProvider("goerli", alchemyApiKey)
-  provider.getBalance(address).then((value) => console.log(value, "value"))
-  const signer = ethers.Wallet.createRandom().connect(provider)
-  console.log(signer, "signer")
-  const slicerContract = new ethers.Contract(
-    slicerAddress,
-    SlicerContract.abi,
-    signer
-  )
-  const getProductAllowed = async () => {
-    const isPayeeAllowed = await slicerContract.isPayeeAllowed(address)
-    console.log(isPayeeAllowed, "isPayeeAllowed")
-  }
-  getProductAllowed()
+  // const provider = new ethers.providers.AlchemyProvider("goerli", alchemyApiKey)
+  // provider.getBalance(address).then((value) => console.log(value, "value"))
+  // const signer = ethers.Wallet.createRandom().connect(provider)
+  // console.log(signer, "signer")
+  // const slicerContract = new ethers.Contract(
+  //   slicerAddress,
+  //   SlicerContract.abi,
+  //   signer
+  // )
+  // const getProductAllowed = async () => {
+  //   const isPayeeAllowed = await slicerContract.isPayeeAllowed(address)
+  //   console.log(isPayeeAllowed, "isPayeeAllowed")
+  // }
+  // getProductAllowed()
 
-  console.log(slicerAddress, "slicer")
-  console.log(`Address ${address}!`)
-  console.log(`Mint ${slices} slices!`)
+  // console.log(slicerAddress, "slicer")
+  // console.log(`Address ${address}!`)
+  // console.log(`Mint ${slices} slices!`)
   console.log("payload", payload)
 } catch (error) {
   core.setFailed(error.message)
