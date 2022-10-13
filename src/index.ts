9fded60c18355b579c64d9b50b0f30e0205905da
Dom-Mac <domenicochad@gmail.com>
2022-10-13 22:58:16 +0200

    workflow

--- src/index.ts
+++ src/index.ts
@@ -28,9 +28,6 @@ export default async function init() {
           // TODO: Add type checks on addresses and sliceAmounts
           // Edit first bot comment
           const comments = await fetch(commentPayload.issue.comments_url)
-          console.log(comments)
-          console.log(comments.message)
-          console.log(await comments.readBody())
 
           // Set bot message to fire in create comment
           botMessage = onRequestMessage(splitText)
