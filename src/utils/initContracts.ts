f23f41954757b27b4f7e34236da41f3c950b66bd
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2022-10-15 21:42:45 +0200

    refactored sliceCore

--- src/utils/initContracts.ts
+++ src/utils/initContracts.ts
@@ -3,13 +3,11 @@ import { SliceCore } from "../types/SliceCore"
 
 const sliceCoreInterface = require("src/abi/SliceCore.json")
 const sliceCoreAddress = "0x21da1b084175f95285B49b22C018889c45E1820d"
+const alchemyId = process.env.ALCHEMY_ID
+const provider = new ethers.providers.AlchemyProvider("mainnet", alchemyId)
 
-export const sliceCore = () => {
-  const alchemyId = process.env.ALCHEMY_ID
-  const provider = new ethers.providers.AlchemyProvider("mainnet", alchemyId)
-  return new ethers.Contract(
-    sliceCoreAddress,
-    sliceCoreInterface.abi,
-    provider
-  ) as SliceCore
-}
+export const sliceCore = new ethers.Contract(
+  sliceCoreAddress,
+  sliceCoreInterface.abi,
+  provider
+) as SliceCore
