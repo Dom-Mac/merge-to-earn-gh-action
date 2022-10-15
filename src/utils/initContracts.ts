99cf9f4d19a1b6206e108a5efe009bf070c5a22b
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2022-10-15 22:25:49 +0200

    fixed sliceCoreInterface

--- src/utils/initContracts.ts
+++ src/utils/initContracts.ts
@@ -1,7 +1,7 @@
 import { ethers } from "ethers"
 import { SliceCore } from "../types/SliceCore"
+import { sliceCoreInterface } from "../abi/SliceCore"
 
-const sliceCoreInterface = require("src/abi/SliceCore.json")
 const sliceCoreAddress = "0x21da1b084175f95285B49b22C018889c45E1820d"
 const alchemyId = process.env.ALCHEMY_ID
 const provider = new ethers.providers.AlchemyProvider("mainnet", alchemyId)
